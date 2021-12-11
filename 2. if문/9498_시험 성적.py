bd3c09e92f736923dabab802f0cd158988d3ca45
K <dkh1001@outlook.kr>
2021-12-11 13:49:26 +0900

    2

---
+++ 2. if문/9498_시험 성적.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+if (n > 89):
+    print("A")
+elif (n > 79):
+    print("B")
+elif (n > 69):
+    print("C")
+elif (n > 59):
+    print("D")
+else:
+    print("F")
