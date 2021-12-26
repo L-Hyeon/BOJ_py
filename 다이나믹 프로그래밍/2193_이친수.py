d60d89fe50c10ead2cb6272028966f2be4abfeef
K <dkh1001@outlook.kr>
2021-12-26 10:50:37 +0900

    x

---
+++ 다이나믹 프로그래밍/2193_이친수.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+def do():
+  N = int(input())
+  if (N == 1):
+    print(1)
+    return
+  memo = [0]*(N + 1)
+  memo [1] = 1
+  memo [2] = 1
+  for i in range(3, N + 1):
+    memo[i] = memo[i - 1] + memo[i - 2]
+  print(memo[N])
+
+do()
