1765356bc186719815a874b24df017cf8d9300ed
K <dkh1001@outlook.kr>
2022-01-08 11:02:22 +0900

    x

--- 다이나믹 프로그래밍/2133_타일 채우기.py
+++ 다이나믹 프로그래밍/2133_타일 채우기.py
@@ -3,13 +3,11 @@ input = sys.stdin.readline
 
 def do():
   N = int(input())
-  if (N == 1):
-    print(1)
-    return
-  memo = [0]*(N + 1)
+  memo = [0]*(31)
   memo[2] = 3
-  for i in range(4, N + 1, 2):
-    memo[i] = 3*memo[i - 2] + 2*sum(memo[0:i - 2:2]) + 2
+  for i in range(4, N + 1):
+    if (i % 2 == 0):
+      memo[i] = 3*memo[i - 2] + 2*sum(memo[0:i - 2:2]) + 2
   print(memo[N])
 
 do()
