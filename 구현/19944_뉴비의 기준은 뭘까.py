82b4b7771a2d1464e72d6184cc8585a58306ef71
K <dkh1001@outlook.kr>
2022-03-29 17:25:26 +0900

    x

---
+++ 구현/19944_뉴비의 기준은 뭘까.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+def do():
+  N, M = map(int, input().split())
+  if (M == 1 or M == 2):
+    print("NEWBIE!")
+  elif (M <= N):
+    print("OLDBIE!")
+  else:
+    print("TLE!")
+
+do()
